436f4a7e69e172e56a41bd5802544e252e802e78
Alexis Hvostal <ahvostal@purdue.edu>
2023-10-29 17:57:21 -0400

    database model and sqlalchemy work

--- backend/database.py
+++ backend/database.py
@@ -1,1 +1,31 @@
 # Connect to Database and define operations
+import os
+from dotenv import load_dotenv
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models import *
+
+#########################
+## Connect to Database ##
+#########################
+load_dotenv(dotenv_path='C:/Users/Lexie/BasketballLeagueManager/.env')
+db_uri = os.getenv("DB_URI")
+if db_uri is None:
+    raise Exception("DB_URI is not defined in the .env file")
+# connection global variable
+session = None
+
+def connect_sqlalchemy():
+    engine = sqlalchemy.create_engine(db_uri, echo=False)
+
+    global session
+    session = scoped_session(sessionmaker(autocommit=False,
+                                          autoflush=False,
+                                          bind=engine))
+    
+    global Base
+    Base.query = session.query_property()
+    Base.metadata.create_all(bind=engine)
+
+connect_sqlalchemy()
--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,46 @@
 # Specify Database Models / Schema
+import sqlalchemy
+from sqlalchemy import Column, Integer, String, Date, Time, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Team(Base):
+    __tablename__='team'
+
+    team_id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    coach = Column(String(30))
+
+class Player(Base):
+    __tablename__='player'
+
+    player_id = Column(Integer, primary_key=True)
+    team_id = Column(Integer, ForeignKey('team.team_id'))
+    first_name = Column(String(30))
+    last_name = Column(String(30))
+    position = Column(String(3))
+    jersey_number = Column(Integer)
+
+class Game(Base):
+    __tablename__='game'
+
+    game_id = Column(Integer, primary_key=True)
+    date = Column(Date)
+    time = Column(Time)
+    location = Column(String(100))
+    home_team_id = Column(Integer, ForeignKey('team.team_id'))
+    home_score = Column(Integer)
+    away_team_id = Column(Integer, ForeignKey('team.team_id'))
+    away_score = Column(Integer)
+
+class Stats(Base):
+    __tablename__='stats'
+
+    player_id = Column(Integer, ForeignKey('player.player_id'), primary_key=True)
+    game_id = Column(Integer, ForeignKey('game.game_id'), primary_key=True)
+    points = Column(Integer)
+    assists = Column(Integer)
+    rebounds = Column(Integer)
+    blocks = Column(Integer)
+    steals = Column(Integer)
