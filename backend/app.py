39ea38394c4c31eb51de4585bd3cf5de7a37fe9a
Alexis Hvostal <ahvostal@purdue.edu>
2023-10-30 16:15:33 -0400

    Backend Flask api routes for CRUD operations on Stats table

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,67 @@
-# Flask API
+import database as db
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Connect to database
+db.connect_sqlalchemy()
+
+@app.route('/')
+def root():
+    return "This is the API for BasketballLeagueManager"
+
+@app.route('/stats/get_stats', methods=['GET'])
+def stats_data():
+    stats = db.get_stats_table()
+    stats_list = [
+        {
+            "player_id": stat.player_id,
+            "game_id": stat.game_id,
+            "points": stat.points,
+            "assists": stat.assists,
+            "rebounds": stat.rebounds,
+            "blocks": stat.blocks,
+            "steals": stat.steals
+        }
+        for stat in stats
+    ]
+    return jsonify(stats_list)
+
+@app.route('/stats/add_stats', methods=['POST'])
+def add_stats():
+    data = request.get_json()
+    db.add_player_stats(
+        data['player_id'],
+        data['game_id'],
+        data['points'],
+        data['assists'],
+        data['rebounds'],
+        data['blocks'],
+        data['steals']
+    )
+
+@app.route('/stats/edit_stats', methods=['PUT'])
+def edit_stats():
+    data = request.json()
+    db.edit_player_stats(
+        data['player_id'],
+        data['game_id'],
+        data['points'],
+        data['assists'],
+        data['rebounds'],
+        data['blocks'],
+        data['steals']
+    )
+
+@app.route('/stats/delete_stats', methods=['DELETE'])
+def delete_stats():
+    data = request.json()
+    db.delete_player_stats(
+        data['player_id'],
+        data['game_id']
+    )
+
+# Main function - connect to DB and start app
+if __name__ == "__main__":
+    db.connect_sqlalchemy()
+    app.run(debug=True, host="localhost", port=5000)
--- backend/database.py
+++ backend/database.py
@@ -11,9 +11,10 @@ from sample_data.games import sample_games
 from sample_data.stats import sample_stats
 
 
-#########################
-## Connect to Database ##
-#########################
+#####################################
+## Connect and Initialize Database ##
+#####################################
+
 load_dotenv(dotenv_path='C:/Users/Lexie/BasketballLeagueManager/.env')
 db_uri = os.getenv("DB_URI")
 if db_uri is None:
@@ -39,7 +40,6 @@ def initialize_tables():
     '''
     Populate database with example instance
     '''
-    connect_sqlalchemy()
     # Add Teams
     teams_to_insert = insert(Team).values(sample_teams)
     session.execute(teams_to_insert)
@@ -57,4 +57,72 @@ def initialize_tables():
     session.execute(stats_to_insert)
     session.commit()
 
-#initialize_tables()
+
+#################################
+## Stats Table CRUD Operations ##
+#################################
+
+def get_stats_table():
+    '''
+    Retrieve all rows in the Stats table
+    '''
+    return Stats.query.all()
+
+def add_player_stats(player_id, game_id, points, assists,
+                     rebounds, blocks, steals):
+    '''
+    Add a player's stats for a game to the Stats table
+    '''
+    new = Stats(player_id=player_id, game_id=game_id, points=points,
+                assists=assists, rebounds=rebounds, blocks=blocks,
+                steals=steals)
+    session.add(new)
+    session.commit()
+
+def edit_player_stats(player_id, game_id, points, assists,
+                      rebounds, blocks, steals):
+    '''
+    Edit a player's stats for a game in the Stats table
+    '''
+    stat = Stats.query.filter_by(player_id=player_id, game_id=game_id).first()
+    stat.points = points
+    stat.assists = assists
+    stat.rebounds = rebounds
+    stat.blocks = blocks
+    stat.steals = steals
+    session.commit()
+
+def delete_player_stats(player_id, game_id):
+    '''
+    Delete a player's stats for a game in the Stats table
+    '''
+    Stats.query.filter_by(player_id=player_id, game_id=game_id).delete()
+    session.commit()
+
+
+#######################
+## Testing Functions ##
+#######################    
+
+def stats_testing():
+    '''
+    Function to test CRUD operations for Stats table
+    '''
+    connect_sqlalchemy()
+    add_player_stats(3, 1, 10, 4, 2, 0, 0)
+    for stat in get_stats_table():
+        print(stat.player_id, stat.game_id, stat.points, stat.assists, 
+              stat.rebounds, stat.blocks, stat.steals)
+    print("######################################")
+    edit_player_stats(3, 1, 5, 5, 5, 5, 5)
+    for stat in get_stats_table():
+        print(stat.player_id, stat.game_id, stat.points, stat.assists, 
+              stat.rebounds, stat.blocks, stat.steals)
+    print("######################################")
+    delete_player_stats(3, 1)
+    for stat in get_stats_table():
+        print(stat.player_id, stat.game_id, stat.points, stat.assists, 
+              stat.rebounds, stat.blocks, stat.steals)
+    print("######################################")
+
+#stats_testing()
